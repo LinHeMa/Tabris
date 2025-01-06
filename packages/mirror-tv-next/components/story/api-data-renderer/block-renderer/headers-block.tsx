6e4612a97a896f9fec26c6dd3fd33d93c07c23e0
LinHeMa <linhema0921@gmail.com>
2025-01-06 15:08:34 +0800

    feat: headers block

---
+++ packages/mirror-tv-next/components/story/api-data-renderer/block-renderer/headers-block.tsx
@@ -0,0 +1,26 @@
+import styles from './_styles/headers-block.module.scss'
+type HeadersBlockPropsType = {
+  data: string[]
+  blockType: string
+}
+const HeadersBlock = ({ data, blockType }: HeadersBlockPropsType) => {
+  const getFirstElement = <T,>(data: T[]) => data[0]
+  const blockContentData = getFirstElement(data)
+  console.log({ blockType })
+
+  const renderHeader = () => {
+    switch (blockType) {
+      case 'header-one':
+        return <h1 dangerouslySetInnerHTML={{ __html: blockContentData }} />
+      case 'header-two':
+        return <h2 dangerouslySetInnerHTML={{ __html: blockContentData }} />
+      default:
+        console.error(`Unhandled block type: ${blockType}`)
+        return null
+    }
+  }
+
+  return <div className={styles.headersBlock}>{renderHeader()}</div>
+}
+
+export default HeadersBlock
